7b06fcbdde5bad98e80a916b5bd550f90c5e823d
taka12natu <hvhpdrmcmtr12@gmail.com>
2021-03-06 21:50:46 +0900

    save

---
+++ atcoder_beginners_selection/ABC049C.rb
@@ -0,0 +1,18 @@
+s = gets.split("")
+arr = ["dream","dreamer","erase","eraser"]
+
+#初めは必ずdrかer
+drの時
+dream XXX
+　dreamの後がeの時
+　dreamer XXX
+　dreamer ase
+　dreamer aser
+
+erの時
+　eraseの後がreraseの時
+　erasererase r
+　erasererase
+その他
+erase XXX（dream,dreamer）
+eraser
