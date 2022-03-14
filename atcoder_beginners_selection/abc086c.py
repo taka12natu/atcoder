8daadabe305774f65e1cce65d64dd4625344bcdc
taka12natu <hvhpdrmcmtr12@gmail.com>
2022-03-14 19:19:06 +0900

    commit

--- atcoder_beginners_selection/abc086c.py
+++ atcoder_beginners_selection/abc086c.py
@@ -6,9 +6,45 @@
 ※１つ前の時点との差で考える
 ※偶数、奇数の条件は不変
 
-１行ずつ判定
+n = int(input())
+flg = False
+pret = 0
+prexy = 0
+for i in range(n):
+  t,x,y = list(map(int, input().split()))
+  if t%2 == (x+y)%2 and t-pret >= x+y-prexy:
+    pret = t
+    prexy = x+y
+    flg = True
+  else:
+    flg = False
+    break
+if flg:
+  print('Yes')
+else:
+  print('No')
+
+
+→テストケース after_contest_01.txt だけクリアできず
  """
 
 n = int(input())
-arr = [list(map(int, input().split())) for i in range(n)]
-print(arr)
+flg = False
+pret = 0
+prex = 0
+prey = 0
+for i in range(n):
+  t,x,y = list(map(int, input().split()))
+  d = abs(x-prex)+abs(y-prey)
+  if ((t-pret)-d)%2 == 0 and t-pret >= d:
+    pret = t
+    prex = x
+    prey = y
+    flg = True
+  else:
+    flg = False
+    break
+if flg:
+  print('Yes')
+else:
+  print('No')
