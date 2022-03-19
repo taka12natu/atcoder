87201f34d217295e0df3915fb2080322f6141065
taka12natu <hvhpdrmcmtr12@gmail.com>
2022-03-20 00:21:28 +0900

    commit

---
+++ past_1/e.py
@@ -0,0 +1,27 @@
+n,q = map(int,input().split())
+user = [["N" for i in range(n)] for i in range(n)]
+for i in range(q):
+  s = list(map(int,input().split()))
+  if s[0] == 1:
+    a = s[1]-1
+    b = s[2]-1
+    user[a][b] = "Y"
+  elif s[0] == 2:
+    a = s[1]-1
+    for index,num in enumerate(user[a]):
+      if user[index][a] == "Y":
+        user[a][index] = "Y"
+  elif s[0] == 3:
+    a = s[1]-1
+    a_follows = []
+    for index,num in enumerate(user[a]): 
+      if num == "Y":
+        a_follows.append(index)
+    for a_follow in a_follows:
+      for index,num in enumerate(user[a_follow]): 
+        if num == "Y" and index != a:
+          user[a][index] = "Y"
+for row in user:
+  print(*row, sep="")
+
+""" user = [list(map(int,input().split())) for i in range(n)] """
